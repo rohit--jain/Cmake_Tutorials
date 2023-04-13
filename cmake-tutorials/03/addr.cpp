eddaa793d3cf5ad7e5fbb1344b2da83d7073d518
rohit--jain <rohit.jain.010@gmail.com>
2023-04-13 14:38:20 +0200

    Sample Lib Testing

---
+++ cmake-tutorials/03/addr.cpp
@@ -0,0 +1,11 @@
+#include "addr.h"
+
+int mearly_math::add(int a, int b)
+{
+    return (a + b);
+}
+
+float mearly_math::add(float a, float b)
+{
+    return (a + b);
+}
---
+++ cmake-tutorials/03/addr.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mearly_math{
+    int add(int a, int b);
+    float add(float a, float b);
+}
